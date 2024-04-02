681e43701ef79fb34faab7651ef01a099835d13d
Akashx1550 <akash1630.be21@chitkara.edu.in>
2024-04-02 19:55:34 +0530

    Created Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Navbar } from './Navbar'
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <>
+      <Navbar />
+      <div className='flex justify-start items-center '>
+        <div className=' flex flex-col ml-20 w-[60%] h-[400px] justify-center items-start'>
+
+          <p className='text-6xl '>Empowering Wellness:
+          </p>
+          <p className='text-5xl pt-2'>Digital Dietitian App</p>
+          <p className='text-2xl pt-8'>Elevate your well-being with DietDex, your personalized digital dietician. Tailored nutrition plans, expert guidance, and science-backed insights help you achieve your health goals effortlessly. Start your journey today
+          </p>
+          <button className='border-2 px-4 py-2 mt-7 bg-blue-500 text-white'>Get started</button>
+        </div>
+        <div >
+          <img className="mt-10" src="https://img.freepik.com/premium-vector/weight-loss-vector-illustration-woman-body-transformation-concept-with-diet-healthy-lifestyle_2175-23018.jpg?w=996" alt="" />
+        </div>
+      </div>
+
+      <div className='flex w-screen justify-center items-center bg-gray-200 h-8'><p className='text-2xl'>Embark on a journey towards a healthier, more empowered lifestyle with our innovative digital dietitian app.
+      </p></div>
+
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <>
+            <div className='flex justify-between items-center h-[50px] bg-gray-700 text-white text-xl list-none'>
+                <div className='ml-4'>
+                    <h1>
+                        DietDex
+                    </h1>
+                </div>
+                <div className='flex gap-10 mr-6 cursor-pointer'>
+                    <li>Home</li>
+                    <li>Contact Us</li>
+                    <li>Diet Program</li>
+                    <li>Login</li>
+                    <li>Get Started</li>
+                </div>
+            </div>
+
+        </>
+    )
+}
