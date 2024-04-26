5bb050b8ad4889d23e74a9b9319c445c5da2f2a1
Akashx1550 <akash1630.be21@chitkara.edu.in>
2024-04-26 19:17:23 +0530

    Added images

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,11 +17,11 @@ export const features = [
       heading: "Easy Meal Planning and Templates",
       desc: "Effortlessly plan and send meal plans to clients with our simple meal planner. No more Word, Excel, or WhatsApp scrolling required to find out which plan you sent. Whatever you sent is recorded for your future reference.",
       texts: [
-        { sub_img: "", sub_texts: "Meal Planning" },
-        { sub_img: "", sub_texts: "Ready to use Templates" },
-        { sub_img: "", sub_texts: "One click send" }
+        { sub_img: "https://static.wixstatic.com/media/197d85_6a9b4ef9117d48f4b64b660680d35228~mv2.png/v1/fill/w_40,h_40,al_c,lg_1,q_85,enc_auto/197d85_6a9b4ef9117d48f4b64b660680d35228~mv2.png", sub_texts: "Meal Planning" },
+        { sub_img: "https://static.wixstatic.com/media/197d85_c090516c6b85437cbdd71c7520f21ef0~mv2.png/v1/fill/w_40,h_40,al_c,lg_1,q_85,enc_auto/197d85_c090516c6b85437cbdd71c7520f21ef0~mv2.png", sub_texts: "Ready to use Templates" },
+        { sub_img: "https://static.wixstatic.com/media/197d85_c578fd073dfa4a09be09c04b08dad528~mv2.png/v1/fill/w_40,h_40,al_c,lg_1,q_85,enc_auto/197d85_c578fd073dfa4a09be09c04b08dad528~mv2.png", sub_texts: "One click send" }
       ],
-      img_id: ""
+      img_id: "https://static.wixstatic.com/media/197d85_b30cb8010e7c45cc82c20aaeec579837~mv2.png/v1/fill/w_460,h_439,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/MealPlanning_edited.png"
     },
     {
       id: 3,
@@ -39,11 +39,11 @@ export const features = [
       heading: "Effective Communication Chat and Video calls",
       desc: "No more scattered client information - our platform keeps everything tidy and in one place",
       texts: [
-        { sub_img: "", sub_texts: "Integrated Chat & Video" },
-        { sub_img: "", sub_texts: "Send Files & Images" },
-        { sub_img: "", sub_texts: "Same as WhatsApp" }
+        { sub_img: "https://static.wixstatic.com/media/197d85_5fdb4ec6531545e183126a35887a180d~mv2.png/v1/fill/w_40,h_40,al_c,lg_1,q_85,enc_auto/197d85_5fdb4ec6531545e183126a35887a180d~mv2.png", sub_texts: "Integrated Chat & Video" },
+        { sub_img: "https://static.wixstatic.com/media/197d85_ce018031cd784c2f8a5a70a97eff51c5~mv2.png/v1/fill/w_40,h_40,al_c,lg_1,q_85,enc_auto/197d85_ce018031cd784c2f8a5a70a97eff51c5~mv2.png", sub_texts: "Send Files & Images" },
+        { sub_img: "https://static.wixstatic.com/media/197d85_d2447a5bc0744a52aa05f6825d4fdb9a~mv2.png/v1/fill/w_40,h_40,al_c,lg_1,q_85,enc_auto/197d85_d2447a5bc0744a52aa05f6825d4fdb9a~mv2.png", sub_texts: "Same as WhatsApp" }
       ],
-      img_id: ""
+      img_id: "https://static.wixstatic.com/media/197d85_7cbb26b130784346872422ce8da261f9~mv2.jpg/v1/fill/w_460,h_439,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/N_RemoteMonitoring.jpg"
     },
     {
       id: 5,
@@ -61,11 +61,11 @@ export const features = [
       heading: "Payment and Reminders",
       desc: "Eliminate the hassle of manually tracking payment timelines and reminding clients with our automated system. No more missed payments or awkward follow-ups - stay organized and on top of everything with ease.",
       texts: [
-        { sub_img: "", sub_texts: "Payment Management" },
-        { sub_img: "", sub_texts: "No Missed Payment" },
-        { sub_img: "", sub_texts: "Payment Simplified" }
+        { sub_img: "https://static.wixstatic.com/media/197d85_07beb859038f40f1912125726fca4adf~mv2.png/v1/fill/w_43,h_43,al_c,lg_1,q_85,enc_auto/icons8-paid-bill-32.png", sub_texts: "Payment Management" },
+        { sub_img: "https://static.wixstatic.com/media/197d85_ccb89ef9b03241b1b1a0b698f60e7e7a~mv2.png/v1/fill/w_43,h_43,al_c,lg_1,q_85,enc_auto/icons8-card-exchange-32.png", sub_texts: "No Missed Payment" },
+        { sub_img: "https://static.wixstatic.com/media/197d85_09988c63d261494b960568844b1c7005~mv2.png/v1/fill/w_40,h_40,al_c,lg_1,q_85,enc_auto/icons8-cashbook-32.png", sub_texts: "Payment Simplified" }
       ],
-      img_id: ""
+      img_id: "https://static.wixstatic.com/media/197d85_769ffed9a6754a1abbfe39625facaea4~mv2.jpg/v1/fill/w_460,h_439,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/N_Payment.jpg"
     },
     {
       id: 7,
